d39bb881995357a43bec1d287979b035f092ae1e
Abiagael <happyluvagho@gmail.com>
2026-02-07 11:50:07 +0100

    first commit

---
+++ heynaya/src/pages/HowItWorks.jsx
@@ -0,0 +1,3 @@
+export default function HowItWorks() {
+  return <div className="p-10">How it works</div>;
+}
---
+++ heynaya/src/pages/Sources.jsx
@@ -0,0 +1,3 @@
+export default function Sources() {
+  return <div className="p-10">Sources</div>;
+}
